1313e90fd973509e64cc55f14e1ccc27037975b8
Paul Querna <pquerna@apache.org>
2012-01-07 16:01:53 -0800

    start on simple echo back of your tenantId and token

--- server.js
+++ server.js
@@ -15,60 +15,6 @@
  *
  */
 
+var server = require('./lib/server');
 
-var express = require('express');
-var http = require('http');
-var os = require('os');
-var path = require('path');
-
-function sslRedirectMiddleware(req, res, next) {
-  var host = req.headers['host'] ? req.headers['host'] : 'cloudmonitoring.nodejitsu.com'; 
-
-  if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] !== 'https') {
-    res.writeHead(301, {'Content-Type': 'text/html', 'Location': 'https://' + host + req.url});
-    res.write('<h1>Hey, use SSL Buddy!</h1>')
-    res.end();
-  }
-  else {
-    next();
-  }
-}
-
-function indexHandler(req, res){
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h1>hello, i know nodejitsu.</h1>')
-  res.end();
-}
-
-function registerUrls(app) {
-  var staticRoot, iconPath, viewsRoot;
-
-  staticRoot = path.join(__dirname, 'static');
-  iconPath = path.join(staticRoot, 'favicon.ico');
-  viewsRoot = path.join(__dirname, 'views');
-
-  app.set('view engine', 'jade');
-  app.set('view options', { layout: false });
-
-  app.use(sslRedirectMiddleware);
-
-  app.get('/favicon.ico', express.favicon(iconPath));
-  app.use('/static/', express['static'](staticRoot));
-
-  app.get('/', indexHandler);
-}
-
-function main() {
-  var port = 80;
-  var app = express.createServer();
-
-  if (os.type() == "Darwin") {
-    port = 8080;
-  }
-
-  registerUrls(app);
-  console.log('Binding to http://0.0.0.0:' + port + '/');
-  app.listen(port, '0.0.0.0');
-}
-
-main();
+server.main();
